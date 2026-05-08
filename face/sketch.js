f6f502f66a53e00373cc038dcf737070f1b68639
janghwanhui874-coder <janghwanhui874@gmail.com>
2026-05-08 16:02:39 +0900

    Add files via upload

---
+++ face/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+
+  background(220);
+  
+ ellipse(200, 200, 290, 340);
+  
+triangle(200, 150, 220, 250, 180, 250);
+  
+   ellipse(130, 170, 80, 40);
+  
+ellipse(270, 170, 80, 40);
+  
+
+}
